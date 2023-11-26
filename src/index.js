31a9e8db7f6780e2cbecd46329a288511f5375fb
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-11-26 15:00:45 +0000

    Work in progress

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const BookList = () => {
+  return <section>
+    <Book/>
+  </section>
+}
+
+const Book = () => {
+  return <article>
+<Image/>
+<Title/>
+<Author/>
+  </article>
+}
+
+
+const Image = () => <h2>Image placeholder</h2>
+const Title = () => <h2>Book Title</h2>
+const Author = () => <h2>Author</h2>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList/>)
+
+
