01d3488a307bae2d5c90feb11ac930dbe5ee72ee
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-11-27 11:29:08 +0000

    progress with project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.css";
 
 const BookList = () => {
-  return <section className='booklist'>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-  </section>
-}
+	return (
+		<section className="booklist">
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
+};
 
 const Book = () => {
-  return <article className='book'>
-<Image/>
-<Title/>
-<Author/>
-  </article>
-}
+	return (
+		<article className="book">
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
 
 const Image = () => (
-  <img src='https://images-eu.ssl-images-amazon.com/images/I/91X6Rzd2VvL._AC_UL900_SR900,600_.jpg' alt='Guinness World Records 2024'
-  />
-)
-const Title = () => <h2>Guinness World Records 2024</h2>
-const Author = () => <h2>Guinness World Records</h2>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BookList/>)
-
-
+	<img
+		src="./images/book-1.jpg"
+		alt="Guinness World Records 2024"
+	/>
+);
+const Title = () => <h2>Guinness World Records 2024</h2>;
+const Author = () => {
+	return (
+		<h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+			Guinness World Records
+		</h4>
+	);
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
