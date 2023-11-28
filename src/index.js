31f00dd059a3fb1f2f454217427be325ef1f7f8a
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-11-28 12:49:16 +0000

    props setup dynamically to an extent

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const author = "Guinness World Records";
-const title = "Guinness World Records 2024";
-const img = "./images/book-1.jpg"
-
+const firstBook = {
+author : "Guinness World Records",
+title : "Guinness World Records 2024", 
+img : "./images/book-1.jpg",
+}
+const secondBook = {
+  author : "Nathan Anthony",
+  title : "Bored of Lunch: The Healthy Air Fryer Book", 
+  img : "https://m.media-amazon.com/images/I/81anCS3GCbL._SY522_.jpg",
+  }
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<Book author={author} title={title} img={img}/>
-			<Book author={author} title={title} img={img}/>
-			<Book author={author} title={title} img={img}/>
-			<Book author={author} title={title} img={img}/>
+			<Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+			<Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
 		</section>
 	);
 };
