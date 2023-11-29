655ab3c9e8a2feb41b3fda8d7f5755448b69f73e
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-11-29 12:47:16 +0000

    object as a prop

--- src/index.js
+++ src/index.js
@@ -8,26 +8,33 @@ const books = [
 		author: "Guinness World Records",
 		title: "Guinness World Records 2024",
 		img: "./images/book-1.jpg",
+		id: 1,
 	},
 	{
 		author: "Nathan Anthony",
 		title: "Bored of Lunch: The Healthy Air Fryer Book",
 		img: "https://m.media-amazon.com/images/I/81anCS3GCbL._SY522_.jpg",
+		id: 2,
 	},
 ];
 
 const BookList = () => {
-	return <section className="booklist">
-    {books.map((book) => {
-      const {img, title, author} = book
-      return (
-        <Book img ={img} title= {title} author= {author} />
-      )
-    })}
-  </section>;
+	return (
+		<section className="booklist">
+			{books.map((book) => {
+				return (
+					<Book
+						{...book}
+            key={book.id}
+					/>
+				);
+			})}
+		</section>
+	);
 };
 const Book = (props) => {
-  const {img, title, author} = props
+	const { img, title, author} = props;
+  console.log(props);
 	return (
 		<article className="book">
 			<img
