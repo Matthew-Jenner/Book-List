d2833a1fac6e16dc8c59c9106737f118ff3b20dd
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-11-30 19:16:16 +0000

    local images in src

--- src/books.js
+++ src/books.js
@@ -1,15 +1,25 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
 export const books = [
 	{
 		author: "Guinness World Records",
 		title: "Guinness World Records 2024",
-		img: "./images/book-1.jpg",
+		img: img1,
 		id: 1,
 	},
 	{
 		author: "Nathan Anthony",
 		title: "Bored of Lunch: The Healthy Air Fryer Book",
-		img: "https://m.media-amazon.com/images/I/81anCS3GCbL._SY522_.jpg",
+		img: img2,
 		id: 2,
+	},
+    {
+		author: "Richard Osman",
+		title: "The Last Devil To Die: The Thursday Murder Club",
+		img: img3,
+		id: 3,
 	},
 ];
 
